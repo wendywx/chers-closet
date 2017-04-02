25e7cf07a4f278d7fb05cca6ce39030a5cb8d5b1
wguo96 <wendywxguo@gmail.com>
2017-04-02 05:02:01 -0400

    modified and populated types database

--- nordstrom/python/insert_data.py
+++ nordstrom/python/insert_data.py
@@ -9,6 +9,67 @@ except:
 
 cur = conn.cursor()
 
+tops = ["tops","tank/cami/shell", "t-shirt/tee", "shirt", "polo", "sweater", \
+"camisole", "blouse/top", "sweatshirt", "sweatshirt/hoody/zipfront",\
+"tunic", "shirtjacket", "swim top", "dress-shirt","sportshirt", "onesie", "sweatsuit/warm-ups"]
+
+bottoms = ["bottoms", "short", "stirrup/legging", "pant", "skirt"]
+
+dresses = ["dresses", "dress", "jumpsuit/romper","skirt set"]
+
+outerwear = ["outerwear","jacket", "blazer", "short jacket/coat", "jacket/coat", \
+"3/4 or long coat", "anorak/parka", "sportcoat", "vest", "raincoat",\
+"poncho/cape", "bolero",]
+
+shoes = ["shoes","sneaker", "flats", "sandals/slides", "loafers", "boots","pumps",\
+"athletic", "mule","clog","slip on","slippers","oxfords"]
+
+junk = ["waist support/band", "panty/brief", "shoe tree","polish",
+"insoles","brush"]
+
+parenttypes = [tops, bottoms, dresses, outerwear, shoes]
+
+
+dressy = ["dressy","onesie","skirt","dress","jumpsuit/romper","skirt set","3/4 or long coat","vest", \
+"bolero","sandals/slides","pumps","mule",]
+
+work = ["work","blouse/top","dress-shirt","pant","blazer","sportcoat","flats","loafers","oxfords"]
+
+casual = ["casual","tank/cami/shell","shirt","polo","sweater","camisole","sweatshirt","tunic", \
+"shirtjacket","short","jacket","short jacket/coat","anorak/parka","raincoat","poncho/cape", \
+"sneaker","boots""clog","slip on","slippers"]
+
+athletic = ["athletic","t-shirt/tee","sweatshirt/hoody/zipfront","swim top","sportshirt", \
+"sweatsuit/warm-ups","stirrup/legging","athletic",]
+
+occasions = [dressy, work, casual, athletic]
+
+
+fall = ["fall","t-shirt/tee","shirt","polo","sweater","blouse/top","sweatshirt","sweatshirt/hoody/zipfront", \
+"tunic","shirtjacket","dress-shirt","sportshirt","sweatsuit/warm-ups","stirrup/legging","pant", \
+"skirt","dress","jumpsuit/romper","skirt set","jacket","blazer","jacket/coat", \
+"3/4 or long coat","anorak/parka","sportcoat","vest","raincoat","poncho/cape","sneaker","flats", \
+"loafers","boots","pumps","athletic","mule","clog","slip on","slippers","oxfords"]
+
+winter = ["wint","shirt","sweater","sweatshirt""sweatshirt/hoody/zipfront","dress-shirt","sportshirt", \
+"stirrup/legging","pant","jacket","blazer","jacket/coat","3/4 or long coat","sportcoat","raincoat", \
+"sneaker","loafers","boots","pumps","athletic","oxfords"]
+
+spring = ["spr", "tank/cami/shell","tops","t-shirt/tee","shirt","polo","blouse/top","sweatshirt/hoody/zipfront", \
+"tunic","shirtjacket","dress-shirt","sportshirt","sweatsuit/warm-ups","short","stirrup/legging", \
+"pant","skirt","dress","jumpsuit/romper","skirt set","blazer","short jacket/coat", \
+"jacket/coat","anorak/parka","sportcoat","vest","raincoat","poncho/cape","bolero","sneaker","flats", \
+"sandals/slides","loafers","pumps","athletic","mule","slip on","slippers","oxfords"]
+
+summer = ["sum","tank/cami/shell","t-shirt/tee","shirt","polo","camisole","blouse/top","tunic","swim top", \
+"dress-shirt","sportshirt","onesie","sweatsuit/warm-ups","short","skirt","dress","jumpsuit/romper", \
+"skirt set","blazer","short jacket/coat","sportcoat","vest","raincoat","poncho/cape","bolero","sneaker", \
+"flats","sandals/slides","pumps","athletic","mule","clog","slip on","oxfords"]
+
+seasons = [fall, spring, summer, winter]
+
+
+
 f = open("data.txt",'r')
 for line in f:
 	a = line.split(",")
@@ -53,17 +114,31 @@ for line in f:
 		parent = a[8]
 		img_url = a[9]
 
-		cur.execute("""
-			INSERT INTO types(typeName,parentType,season)
-			VALUES ('{0}',{1},{2})
-			ON CONFLICT DO NOTHING;
-		""".format(product_type,'null','null'))
+
+		for ptype in parenttypes:
+			if product_type.lower() in ptype:
+				parent_type = ptype[0]
+		
+		for occasion in occasions:
+			if product_type.lower() in occasion:
+				occasion_type = occasion[0]
+
+		product_seasons = ""
+		for season in seasons:
+			if product_type.lower() in season:
+				product_seasons += season[0]
 
 		cur.execute("""
-			INSERT INTO products(productId,productName,productType,brand,color,gender,price,rating,imgurl)
-			VALUES ({0},'{1}','{2}','{3}','{4}','{5}',{6},{7},'{8}')
-			ON CONFLICT DO NOTHING;
-		""".format(product_id,product_name,product_type,brand,color,gender,price,rating,img_url))
+		 	INSERT INTO types(typeName,parentType,season,occasion)
+		 	VALUES ('{0}','{1}','{2}','{3}')
+		 	ON CONFLICT DO NOTHING;
+		""".format(product_type, parent_type, product_seasons, occasion_type))
+
+		# cur.execute("""
+		# 	INSERT INTO products(productId,productName,productType,brand,color,gender,price,rating,imgurl)
+		# 	VALUES ({0},'{1}','{2}','{3}','{4}','{5}',{6},{7},'{8}')
+		# 	ON CONFLICT DO NOTHING;
+		# """.format(product_id,product_name,product_type,brand,color,gender,price,rating,img_url))
 		
 
 
