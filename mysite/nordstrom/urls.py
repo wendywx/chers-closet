bfe0ce6893457f4812ea4146a196110359498318
wguo96 <wendywxguo@gmail.com>
2017-04-14 17:12:40 -0400

    modified templates to work with django

--- mysite/nordstrom/urls.py
+++ mysite/nordstrom/urls.py
@@ -6,4 +6,13 @@ urlpatterns = [
     url(r'^$', views.home, name = 'home'),
     url(r'^home.html$', views.home, name = 'home'),
     url(r'^browse_products.html$', views.browse_products, name = 'browse_products'),
+    url(r'^about.html$', views.about, name = 'about'),
+    url(r'^acct_home.html$', views.acct_home, name = 'acct_home'),
+    url(r'^contact.html$', views.contact, name = 'contact'),
+    url(r'^create_new_closet.html$', views.create_new_closet, name = 'create_new_closet'),
+    url(r'^create_new_outfit.html$', views.create_new_outfit, name = 'create_new_outfit'),
+    url(r'^log_in.html$', views.log_in, name = 'log_in'),
+    url(r'^my_account.html$', views.my_account, name = 'my_account'),
+    url(r'^register.html$', views.register, name = 'register'),
+    url(r'^view_closet.html$', views.view_closet, name = 'view_closet'),
 ]
