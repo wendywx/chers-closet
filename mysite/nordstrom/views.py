f3e42d56008eb9950d57f463c69c867339699788
Anna Pelleti <annapelleti@Annas-MacBook-Air-3.local>
2017-04-19 19:13:47 -0400

    adds products to closet

--- mysite/nordstrom/views.py
+++ mysite/nordstrom/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Product, Closet
+from .models import Product, Closet, Type
 
 def home(request):
 	return render(request, 'home.html', {})
 
 def browse_products(request):
-	return render(request, 'browse_products.html', {})
+	result = Type.objects.all()
+	return render(request, 'browse_products.html', {"results": result})
 
 def product_results(request):
 	return render(request, 'product_results.html', {})
@@ -18,29 +19,34 @@ def added_to_closet(request):
 def addToCloset(request):
 	addedproductid = request.GET.get('newproductid')
 	myclosetid = request.GET.get('closetid')
+	closet_query=Closet.objects.filter(closetid=myclosetid).values('product1')
+
 	mycloset, created = Closet.objects.update_or_create(closetid=myclosetid, defaults={'closetid':myclosetid})
-	null_closet(mycloset)
+	if(created):
+		null_closet(myclosetid)
+
 	colnum = 1
 	cname = "product" + str(colnum)
 	query =  Closet.objects.filter(closetid=myclosetid).values(cname)
-	pid =int(query[0][cname]) 
+
+	pid =query[0][cname]
+
 	
-	while pid is not 0 and pid < 50:
+	while pid is not 0 and colnum < 51:
 		colnum += 1
 		cname = "product" + str(colnum)
 		query =  Closet.objects.filter(closetid=myclosetid).values(cname)
 		pid =int(query[0][cname]) 
-	if pid is 0:
-		Closet.objects.filter(closetid=myclosetid).update(**{cname: addedproductid})
-		closet_query=Closet.objects.filter(closetid=myclosetid).values(cname)	
 
-		#return the product name and imgurl to the html
+	if pid is 0:
+
+		Closet.objects.filter(closetid=myclosetid).update(**{cname: addedproductid})
+
+		closet_query=Closet.objects.filter(closetid=myclosetid).values(cname)	
 		result = [addedproductid, myclosetid, closet_query[0][cname]]
 	else:
 		print("closet is full")
 		result = ["closet is full"]
-
-	#have to loop through every single product...
 
 	return render(request, "added_to_closet.html", {"results": result})
 
@@ -96,53 +102,6 @@ def view_closet(request):
 	return render(request, 'view_closet.html', {})
 
 def null_closet(mycloset):
-	mycloset.product1 = 0
-	mycloset.product2 = 0
-	mycloset.product3 = 0
-	mycloset.product4 = 0
-	mycloset.product5 = 0
-	mycloset.product6 = 0
-	mycloset.product7 = 0
-	mycloset.product8 = 0
-	mycloset.product9 = 0
-	mycloset.product10 = 0
-	mycloset.product11 = 0
-	mycloset.product12 = 0
-	mycloset.product13 = 0
-	mycloset.product14 = 0
-	mycloset.product15 = 0
-	mycloset.product16 = 0
-	mycloset.product17 = 0
-	mycloset.product18 = 0
-	mycloset.product19 = 0
-	mycloset.product20 = 0
-	mycloset.product21 = 0
-	mycloset.product22 = 0
-	mycloset.product23 = 0
-	mycloset.product24 = 0
-	mycloset.product25 = 0
-	mycloset.product26 = 0
-	mycloset.product27 = 0
-	mycloset.product28 = 0
-	mycloset.product29 = 0	
-	mycloset.product30 = 0
-	mycloset.product31 = 0
-	mycloset.product32 = 0
-	mycloset.product33 = 0
-	mycloset.product34 = 0
-	mycloset.product35 = 0
-	mycloset.product36 = 0
-	mycloset.product37 = 0
-	mycloset.product38 = 0
-	mycloset.product39 = 0
-	mycloset.product40 = 0
-	mycloset.product41 = 0
-	mycloset.product42 = 0
-	mycloset.product43 = 0
-	mycloset.product44 = 0
-	mycloset.product45 = 0
-	mycloset.product46 = 0
-	mycloset.product47 = 0
-	mycloset.product48 = 0
-	mycloset.product49 = 0
-	mycloset.product50 = 0	
+	for i in range(1,51):
+		cname = "product" + str(i)
+		Closet.objects.filter(closetid=mycloset).update(**{cname: 0})
