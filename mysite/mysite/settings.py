0a395d8d5360e0624ca654d7698293a490f0d598
Anna Pelleti <annapelleti@Annas-MacBook-Air-3.local>
2017-04-24 23:38:07 -0400

    initial CSS changesincluding addition of static folder

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,15 +81,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-<<<<<<< HEAD
-        'NAME': 'Wendy',
-        'USER': 'Wendy',
-        'PASSWORD': 'Wendy',
-=======
-        'NAME': 'djangosite',
+        'NAME': 'nordstrom',
         'USER': 'annapelleti',
         'PASSWORD': 'annapelleti',
->>>>>>> 6aebc4d9fd456c7660b2481fe194a0968ee50933
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -131,5 +125,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+
